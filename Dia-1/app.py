9175245b05e9dea13c7cc952b1cb4bd6acc3b8f1
JohannHenry <johan88henry@gmail.com>
2023-03-09 23:18:55 -0500

    S4 migrations, dto

--- Dia-1/app.py
+++ Dia-1/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
+from bd import conexion
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:powermusic@localhost:5432/tareas'
+conexion.init_app(app)
+
+Migrate(app = app,db = conexion)
 
 if __name__== '__main__':
     app.run(debug=True)
