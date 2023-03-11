fb72cb7e95f49ad4d43102af1be7030a5fbee4e0
JohannHenry <johan88henry@gmail.com>
2023-03-10 21:20:01 -0500

    S4 postman, modificar

---
+++ Dia-1/controllers/usuario_controller.py
@@ -0,0 +1,76 @@
+from flask_restful import Resource, request
+from bcrypt import hashpw, gensalt, checkpw
+from sqlalchemy.orm import Query
+from flask_jwt_extended import create_access_token
+from dtos.usuario_dto import UsuarioDto, LoginDto
+from models.usuario_model import Usuario
+from bd import conexion
+
+class UsuariosController(Resource):
+    def post(self):
+        data = request.json
+        dto = UsuarioDto()
+        try:
+            data_validada = dto.load(data)
+            # Generar el hash de la password
+            salt = gensalt(rounds=10)
+            password = bytes(data_validada.get('password'), 'utf-8')
+            password_hashed = hashpw(password, salt)
+            password_hashed_str = password_hashed.decode('utf-8')
+
+            nuevo_usuario = Usuario(
+                correo = data_validada.get('correo'),
+                password = password_hashed_str,
+                nombre = data_validada.get('nombre'),
+                apellido = data_validada.get('apellido'))
+            
+            conexion.session.add(nuevo_usuario)
+            conexion.session.commit()
+
+            return {
+                'message': 'Usuario creado exitosamente'
+            }
+            
+
+        except Exception as error:
+            return {
+                'message': 'Error al ingresar el usuario',
+                'content': error.args
+            }
+        
+class LoginController(Resource):
+    def post(self):
+        data = request.json
+        dto = LoginDto()
+        try:
+            data_validada = dto.load(data)
+            # Buscar el usuario por ese correo
+            query:Query = conexion.session.query(Usuario)
+            usuario_encontrado: Usuario | None = query.filter_by(correo = data_validada.get('correo')).first()
+            if not usuario_encontrado:
+                return {
+                    'message': 'El usuario no existe'
+                }
+            # Ahora valido si la contraseña es la correcta
+            # Lo convierto a bytes por que asi se trabaja la funcion checkpw
+            hashed_password = bytes(usuario_encontrado.password, 'utf-8')
+            password = bytes(data_validada.get('password'), 'utf-8')
+
+            resultado = checkpw(password,hashed_password)
+
+            if resultado:
+                # identify > sirve para indicar a que usuario le pertenece a esta JWT
+                token = create_access_token(identify = usuario_encontrado.id)
+                return {
+                    'content': token
+                }
+            else:
+                return{
+                    'message': 'Credenciales incorrectas'
+                }
+
+        except Exception as error:
+            return {
+                'message': 'Error al hacer el login',
+                'content': error.args
+            }
---
+++ Dia-1/dtos/usuario_dto.py
@@ -0,0 +1,12 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import Schema, fields
+from models.usuario_model import Usuario
+
+class UsuarioDto(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Usuario
+
+        class LoginDto(Schema):
+            # validar el siguiente patron (expresion regular) xxxxxxx@xxxxx.xx
+            correo = fields.Email(required=True, error_messages= {'required': 'El correo debe ser requerido'})
+            password = fields.Str(required=True, error_messages={'required': 'El password debe ser requerido'})
