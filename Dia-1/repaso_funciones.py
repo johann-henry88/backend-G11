e59d6450d7e42175129500ee376f75a02eb6d527
JohannHenry <johan88henry@gmail.com>
2023-03-15 20:16:22 -0500

    S4 cors

---
+++ Dia-1/repaso_funciones.py
@@ -0,0 +1,12 @@
+def sumar(num1, num2):
+    return num1 + num2
+resultado = sumar(1,2)
+print(resultado)
+
+resultado2 = sumar(num1=1, num2=2)
+print(resultado2)
+
+parametros = {
+    'num1' : 5,
+    'num2' : 8,
+}
