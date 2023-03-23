4102280a55aff7048280ac32bcea356fdd94e6d5
JohannHenry <johan88henry@gmail.com>
2023-03-22 19:44:42 -0500

    agregue gunicorn

--- controllers/producto_controller.py
+++ controllers/producto_controller.py
@@ -1,9 +1,10 @@
 from flask_restful import Resource, request
 from db import conexion
 from models.categoria_model import Producto
-from dtos.producto_dto import ProductoDto
+from dtos.producto_dto import ProductoDto, MostrarProductoDto
 from os import path
-
+from werkzeug.utils import secure_filename
+from uuid import uuid4
 class ProductosController(Resource):
     def post(self):
         data = request.form
@@ -12,9 +13,20 @@ class ProductosController(Resource):
         imagen = request.files.get('imagen')
         # TODO: agregar un uuid al nombre de la imagen y sea un nombre valido
         # TODO: no recibir imagenes que pesen mas de 10MB
-        data['imagen'] = imagen.filename
+        #                  B  >  KB  >  MB
+        CONTENIDO_MAXIMO = 10 * 1024 * 1024 
         try:
+            if imagen is None:  
+                raise Exception("Se necesita una imagen para poder crear el producto")
+            
+            if  mimetype_valido not in imagen.mimetype:
+                raise Exception('El archivo no es un archivo valido')
+            
+            if imagen.content_length > CONTENIDO_MAXIMO:
+                raise Exception('El archivo supera los 10Mb')
+            
             dto = ProductoDto()
+            nombre_seguro = secure_filename(uuid4().hex + '-' + imagen.filename)
             data_serializada = dto.load(data)
             nuevo_producto = Producto(**data_serializada)
 
