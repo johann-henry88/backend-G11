f51d3f241a0f4eda0dd480278f6ca6535105f26d
JohannHenry <johan88henry@gmail.com>
2023-04-14 22:41:59 -0500

    update  prisma

---
+++ express_con_prisma/src/routes/categorias.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { 
+    actualizarCategoria,
+    crearCategoria, 
+    devolverCategoria, 
+    eliminarCategoria, 
+    listarCategoria, 
+} from "../controllers/categorias.controller.js";
+
+export const categoriaRouter = Router();
+
+categoriaRouter.route("/categorias").post(crearCategoria).get(listarCategoria);
+
+categoriaRouter
+  .route("/categoria/:id")
+  .get(devolverCategoria)
+  .patch(actualizarCategoria)
+  .put(actualizarCategoria)
+  .delete(eliminarCategoria);
