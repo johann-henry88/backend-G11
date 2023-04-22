929553b62d36f09486d93d1ae85bc4aa1c94b71b
JohannHenry <johan88henry@gmail.com>
2023-04-21 23:22:50 -0500

    S10 utils,s3, controllers

---
+++ src/routes/eventos.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import * as controller from "../controllers/eventos.controllers.js"
+
+export const eventoRouter = Router();
+
+eventoRouter.route("/eventos").post(controller.crearEvento);
+eventoRouter.route("/prueba").get(controller.probarS3);
---
+++ src/utils/wachiman.js
@@ -0,0 +1,7 @@
+import jsonwebtoken from "jsonwebtoken";
+
+export const validarToken = (req, res, next) => {
+    //TODO 1ra parte validar que hay el encabezado de autorización
+  // Validar que la ficha sea correcta
+  // agregar al req.user la informacion del usuario de la token
+}
